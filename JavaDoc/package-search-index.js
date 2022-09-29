6eefa5222b04d074b448253c0c5765ada77ba1d4
darkbuckwheat <74707285+darkbuckwheat@users.noreply.github.com>
2022-09-29 21:18:05 +0300

    Delete JavaDoc directory

--- JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Classes"},{"l":"Commands"},{"l":"Creators"},{"l":"Exceptions"},{"l":"JSONstaff"},{"l":"Managers"}];updateSearchResults();
